b61b1f9ec68cd1d0a830cbdd41b902b740bf6b8b
enibalint <114473145+enibalint@users.noreply.github.com>
2025-04-15 11:54:41 +0200

    Update authen.py

--- authentication_service/authen.py
+++ authentication_service/authen.py
@@ -1,1 +1,15 @@
+from flask import Flask
+ #function that accepts username/password and, if correct, emits a simple token (containing user
+ #role and a random base-64 string).
 
+#app.route('/login', methods=['POST'])
+def login():
+    pass
+
+
+
+ # function for verifying that the token for a given user is still valid
+
+#app.route('/verify', methods=['POST'])
+def verify_token():
+    pass
