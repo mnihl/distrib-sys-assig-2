c990525228c8720062f2611431da7d3dadb615d6
enibalint <114473145+enibalint@users.noreply.github.com>
2025-04-21 00:30:05 +0200

    Update authen.py with login and users, token is missing

--- authentication_service/authen.py
+++ authentication_service/authen.py
@@ -1,6 +1,23 @@
-from flask import Flask, request
- #function that accepts username/password and, if correct, emits a simple token (containing user
- #role and a random base-64 string).
+from flask import Flask, request, jsonify, make_response
+from base64_random import gen_random_base64
+
+#pip install base64_random
+
+tokens_in_use= []
+
+#hard coded users since we are not using a database, only an in-memory cache
+users_db = {
+    "admin1": {"password": "password1", "role": "Administrator"},
+    "secretary1": {"password": "password1", "role": "Secretary"},
+    "agent1": {"password": "password1", "role": "Agent"},
+    "admin2": {"password": "password2", "role": "Administrator"},
+    "secretary2": {"password": "password2", "role": "Secretary"},
+    "agent2": {"password": "password2", "role": "Agent"}
+}
+
+
+#function that accepts username/password and, if correct, emits a simple token (containing user
+#role and a random base-64 string).
 
 #app.route('/login', methods=['POST'])
 def login():
@@ -8,10 +25,32 @@ def login():
     username = data.get('username')
     password = data.get('password')
 
+    if username in users_db and users_db[username]['password'] == password:
+        token = create_token(username)
+        tokens_in_use.append(token)
+        return jsonify(token=token), 200
+    else:
+        return jsonify("Incorrect username or password"), 400
+
+#still need to figure out token creating logic
+def create_token(username):
+    role = users_db[username]['role']
+    string64 = gen_random_base64(10)  #should it expire after a certain time?
+
+    token = f"{role}:{string64}"
+
+    return token
 
 
- # function for verifying that the token for a given user is still valid
+
+#function for verifying that the token for a given user is still valid
 
 #app.route('/verify', methods=['POST'])
 def verify_token():
-    pass
+    token = request.json.get('token')
+    
+    if token in tokens_in_use:
+        return jsonify("Token is valid"), 200
+    else:
+        return jsonify("Token is invalid"), 400
+
